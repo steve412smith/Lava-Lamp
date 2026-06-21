8b10cb07c73e5782853e34112056d3e31f4e844c
steve412smith <steve412smith@gmail.com>
2026-06-20 23:06:41 -0500

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,16 @@
+const CACHE = 'lava-drift-v2.2.0';
+const ASSETS = ['./', './index.html', './style.css?v=2.2.0', './app.js?v=2.2.0', './manifest.json', './icon.svg'];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE).then(cache => cache.put(event.request, copy));
+    return response;
+  }).catch(() => caches.match(event.request)));
+});
